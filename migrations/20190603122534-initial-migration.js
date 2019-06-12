c657a93c2d86cd0211dec364beb371e0b6d36942
Louis Slater <louismcs@hotmail.co.uk>
2019-06-12 12:26:40 +0100

    Prettified

--- migrations/20190603122534-initial-migration.js
+++ migrations/20190603122534-initial-migration.js
@@ -5,26 +5,30 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
 };
 
-exports.up = function (db, callback) {
-  db.createTable('pets', {
-    id: { type: 'int', primaryKey: true },
-    name: 'string'
-  }, callback);
+exports.up = function(db, callback) {
+    db.createTable(
+        'pets',
+        {
+            id: { type: 'int', primaryKey: true },
+            name: 'string'
+        },
+        callback
+    );
 };
 
-exports.down = function (db, callback) {
-  db.dropTable('pets', callback);
+exports.down = function(db, callback) {
+    db.dropTable('pets', callback);
 };
 
 exports._meta = {
-  "version": 1
+    version: 1
 };
--- migrations/20190603153818-add-sample-data.js
+++ migrations/20190603153818-add-sample-data.js
@@ -5,24 +5,24 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
 };
 
 exports.up = function(db, callback) {
-  db.runSql(`insert into pets (id, name) values (1, 'roverrr')`, callback);
-  // db.insert("pets", ["id", "name"], ["1", "rover"], callback);
+    db.runSql(`insert into pets (id, name) values (1, 'roverrr')`, callback);
+    // db.insert("pets", ["id", "name"], ["1", "rover"], callback);
 };
 
 exports.down = function(db, callback) {
-  db.runSql(`DELETE FROM pets WHERE name='roverrr';`, callback);
+    db.runSql(`DELETE FROM pets WHERE name='roverrr';`, callback);
 };
 
 exports._meta = {
-  "version": 1
+    version: 1
 };
