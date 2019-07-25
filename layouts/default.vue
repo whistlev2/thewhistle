998a1c8516aee77349ac14f9b720a565dc3cd0de
Tom Berman <tjcberman@gmail.com>
2019-07-25 13:29:16 +0100

    Bring Nuxt framework into project

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app light>
+
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/reports.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-data-table
+  :headers="headers"
+  :items="body"
+  :items-per-page="5"
+  class="elevation-1"
+></v-data-table>
+</template>
+
+
+<script>
+var _ = require('underscore');
+
+export default {
+  asyncData (context) {
+    var headers = []
+    var answers = []
+    _.each(context.responses[0].definition.fields, function(field) { headers.push({text: field.title, value: field.id})});
+
+    _.each(context.responses, function(response) {
+      var res = {}
+      _.each(headers, function(col) {
+        res[col.value] = 33 // Function to lookup the value from the ID
+      })
+      answers.push(res)
+    })
+
+    return {
+      headers: headers,
+      body: answers
+    }
+  },
+  fetch () {
+  },
+  head () {
+    // Set Meta Tags for this Page
+  },
+  // and more functionality to discover
+
+}
+</script>
