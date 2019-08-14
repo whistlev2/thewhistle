f42ecfb3a2ac16cc65a44cd745cbbc515cb494a5
Louis Slater <louismcs@hotmail.co.uk>
2019-08-14 11:56:38 +0100

    Basic low tech branching

---
+++ components/questions/Question.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p id="my-embedded-typeform"> HELLO</p>
+  </div>
+</template>
+
+<<script>
+export default {
+  mounted() {
+    console.log(context.json);
+ }
+}
+</script>
--- middleware/load.js
+++ middleware/load.js
@@ -1,20 +1,29 @@
-import {loadReport, extractTestSurvey} from './testData'
+import {
+    loadReport,
+    extractTestSurvey
+} from './testData'
+
+import Lowtech from './lowtech';
 
 export default function (context) {
-  var path = context.route.params;
-  // console.log(path)
+    var path = context.route.params;
+    // console.log(path)
 
-  if(path.hasOwnProperty('reports')) {
-    context.responses = loadReport(path.reports);
-  }
+    if (path.hasOwnProperty('reports')) {
+        context.responses = loadReport(path.reports);
+    }
 
-  if(path.hasOwnProperty('survey')) {
-    context.survey = path.survey;//extractTestSurvey(path.survey);
-  }
+    if (path.hasOwnProperty('survey')) {
+        context.survey = path.survey; //extractTestSurvey(path.survey);
+    }
 
-  if(path.hasOwnProperty('htmlform')) {
-    context.survey = extractTestSurvey(path.survey);
-  }
+    if (path.hasOwnProperty('htmlform')) {
+        context.survey = extractTestSurvey(path.survey);
+    }
 
-  return context
+    if (path.hasOwnProperty('lowtech')) {
+        context.questions = Lowtech.getNextQuestions(path.lowtech);
+    }
+
+    return context
 }
