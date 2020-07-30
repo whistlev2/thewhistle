d321ed9682ce4d0c426cb0f9a460940a663cf92e
Louis Slater <louismcs@hotmail.co.uk>
2020-07-30 02:39:10 +0100

    Add first question front end

--- pages/edit-form/_form.vue
+++ pages/edit-form/_form.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
         <h1>Edit Form</h1>
-        <!-- Add First Question button if no questions -->
+        <template v-if="form.length == 0">
+            <v-btn x-large outlined v-on:click="openAddQuestionModal" class="blueBtn">Add first question</v-btn>
+            <AddQuestionModal :show="showAddQuestionModal" @close="closeAddQuestionModal" @submit="addQuestion" :newQuestion="newQuestion" />
+        </template>
         <EditQuestion v-for="question in form" :question="question" :slug="slug" :key="question.ref" v-on:questionChange="updateForm" />      
     </div>
 </template>
 
+<style scoped>
+.blueBtn {
+    background-color: #50addb;
+    color: white;
+}
+</style>
+
 <script>
 // TODO - L - Form editing
 import EditQuestion from '../../components/editQuestion/EditQuestion.vue'
+import AddQuestionModal from './AddQuestionModal.vue';
+
 export default {
     components: {
-        EditQuestion
+        EditQuestion,
+        AddQuestionModal
     },
 
     asyncData(context) {
@@ -22,9 +35,36 @@ export default {
         }
     },
     
+    data() {
+        return {
+            showAddQuestionModal: false,
+            newQuestion: {}
+        }
+    },
+
     methods: {
+        addQuestion() {
+            let url = `/api/edit-form/${this.$attrs.slug}/add-first-question`;
+            let data = {
+                before: this.addBefore,
+                question: this.newQuestion
+            };
+            axios.post(url, data).then((response) => {
+                this.updateForm(response.data.form);  
+                //TODO: Handle errors
+            });
+        },
+
         updateForm(form) {
             this.form = form;
+        },
+
+        openAddOptionModal() {
+            this.showAddOptionModal = true;
+        },
+
+        closeAddOptionModal() {
+            this.showAddOptionModal = false;
         }
     }
 
