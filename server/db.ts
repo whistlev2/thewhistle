a9aac4ee8b456d7e7c5247ca4efd55119deaa794
Louis Slater <louismcs@hotmail.co.uk>
2020-08-06 14:28:49 +0100

    Get db info from environment vars

--- server/db.ts
+++ server/db.ts
@@ -4,10 +4,10 @@ var db = null
 
 if (process.env.NODE_ENV === 'production') {
     db = new Pool({
-        user: 'cwopzhgecnuozu',
-        host: 'ec2-79-125-4-72.eu-west-1.compute.amazonaws.com',
-        database: 'd37eu06rjqd573',
-        password: '2a8604eab1f84793080726fd221764a225f0d5a16131760cf5a5362c24f4b766',
+        user: process.env.DATABASE_URL,
+        host: process.env.DATABASE_URL,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
         port: 5432,
         ssl: true
     });
