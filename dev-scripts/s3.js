9fb9ff8c575a6886fea9f4374304dbeda17860cc
Louis Slater <louismcs@hotmail.co.uk>
2020-09-10 11:12:35 +0100

    S3 dev script

---
+++ dev-scripts/s3.js
@@ -0,0 +1,20 @@
+var AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var credentials = {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey : process.env.AWS_SECRET_KEY
+};
+AWS.config.update({credentials: credentials, region: 'eu-west-2'});
+var s3 = new AWS.S3();
+
+var presignedGETURL = s3.getSignedUrl('getObject', {
+    Bucket: process.env.AWS_BUCKET,
+    Key: '', //filename
+    Expires: 1000 //time to expire in seconds
+});
+
+console.log(presignedGETURL);
+//heroku pg:backups:restore --app staging-thewhistle --confirm "" DATABASE_URL
